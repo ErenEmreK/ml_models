2cbbe5efe0ca84f00bd49a5e324c0cbf2a5fd002
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-02-21 18:25:53 +0300

    general structure done

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,10 +1,21 @@
 import numpy as np
 
+class DecisionNode:
+    def __init__(self, split_feature, split_value, left_subtree, right_subtree):
+        self.split_feature = split_feature  
+        self.split_value = split_value 
+        self.left_subtree = left_subtree  
+        self.right_subtree = right_subtree  
+        
+class LeafNode:
+    def __init__(self, majority_label):
+        self.majority_label = majority_label
+        
 class DecisionTree:
     def __init__(self):
         pass
     
-    def find_best_split(self):
+    def find_best_split(self, X, y):
         """
         Evaluate all possible splits for each feature.
         Calculate impurity measures (e.g., Gini impurity, entropy) for each split.
@@ -19,15 +30,32 @@ class DecisionTree:
     def create_leaf_node(self):
         pass
     
-    def stopping_criteria(self, data, max_depth):
+    def stopping_criteria(self, X, y, max_depth):
+        pass
+    
+    def most_common_class(self, y):
         pass
         
-    def build_tree(self, data, target, max_depth):
+    def build_tree(self, X, y, target, max_depth):
+        #check if a stopping criteria is met
+        #return node as a leaf node(terminal node)
+        if self.stopping_criteria(X, y, max_depth):
+            return LeafNode(self.most_common_class(y))
         
-        if self.stopping_criteria(data, max_depth):
-            return self.create_leaf_node()
+        #find best split using gini/purity vs.
+        split_feature, split_value = self.find_best_split(X, y)
         
-        split_feature = self.find_best_split()
-        left, right = self.split_data(split_feature)
+        if split_feature is None:
+            return LeafNode(self.most_common_class(y))
         
+        left_X, left_y, right_X, right_y = self.split_data(split_feature)
         
+        left_subtree = self.build_tree(left_X, left_y, target, max_depth - 1)
+        right_subtree = self.build_tree(right_X, right_y, target, max_depth - 1)
+        
+        #create new decision node
+        #(since first decision node (root node) will be returned at last,
+        #this function will return root node)
+        return DecisionNode(split_feature, split_value, left_subtree, right_subtree)
+    
+    
